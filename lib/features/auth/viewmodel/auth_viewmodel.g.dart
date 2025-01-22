4be3ed5960bfd2ec4499912af742fdd075e30fe5
Vivek <rajawatshivam55@gmail.com>
2025-01-22 10:36:21 +0530

    initial commit

---
+++ lib/features/auth/viewmodel/auth_viewmodel.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_viewmodel.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$authViewmodelHash() => r'4a76de191621f930447f3838345d7cee8f54b1cb';
+
+/// See also [AuthViewmodel].
+@ProviderFor(AuthViewmodel)
+final authViewmodelProvider =
+    AutoDisposeNotifierProvider<AuthViewmodel, AsyncValue<UserModel>?>.internal(
+  AuthViewmodel.new,
+  name: r'authViewmodelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$authViewmodelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AuthViewmodel = AutoDisposeNotifier<AsyncValue<UserModel>?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
